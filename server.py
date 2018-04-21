1cdcee2e1d36cc8e4980894e13693373f10b8702
Hong <taeook03@gmail.com>
2018-04-21 15:38:47 +0900

    keyboard

--- server.py
+++ server.py
@@ -5,3 +5,20 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return "hello world"
+
+@app.route('/keyboard')
+def keyboard():
+	data = {
+		"type":"buttons",
+		"buttons": [
+			"버튼1", "버튼2", "버튼3"
+		]
+	}
+	res = jsonify(data)
+	return res
+
+def jsonify(obj):
+js = json.dumps(obj)
+res = make_response(js)
+res.headers['Content-Type'] = 'application/json'
+return res
